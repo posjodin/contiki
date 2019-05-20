544cf6fac51731c31e906d3c20024735ecbb0a82
Goradux <35964674+Goradux@users.noreply.github.com>
2019-05-20 17:20:35 +0200

    skeleton

--- examples/avr-rss2/mbus/mbus_main.c
+++ examples/avr-rss2/mbus/mbus_main.c
@@ -1,5 +1,13 @@
 
+/*
+1. create a mbus_frame
+2. set baud address
+3. try to write to the usart1
 
+
+
+
+*/
 
 
 int
@@ -22,5 +30,23 @@ int
 mbus_scan(int address, int baudrate, int debug)
 {
   uint8_t frame[5] = {0x10, 0x40, 0x43, 0x83, 0x16};
-  uint8_t frame[5] = {MBUS_FRAME_SHORT_START, , , , }
+
+  int short2 = 0;
+  int short3 = 0;
+  int short4 = short2 + short3;
+  uint8_t frame[5] = {MBUS_FRAME_SHORT_START, short2, short3, short4, MBUS_FRAME_STOP}
+
+  for (i = 0; i < 5; i++) {
+    usart1_tx(&frame[i], 1);      //rewrite tx so it takes only 1 argument
+  }
+
+}
+
+int
+mbus_scan_all(int baudrate, int debug)
+{
+  for (int i = 0; i <= 250; i++)
+  {
+    mbus_scan(i, baudrate, debug);
+  }
 }
