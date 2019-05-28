31117bb113ca4577df84033ef317bac0a87299d0
Goradux <abbbo@mail.ru>
2019-05-28 11:25:09 +0200

    fix

--- examples/avr-rss2/mbus/hello-mbus.c
+++ examples/avr-rss2/mbus/hello-mbus.c
@@ -92,7 +92,7 @@ PROCESS_THREAD(hello_mbus_process, ev, data)
     mbus_request_data_at_primary_address(67, mbus_data, MBUS_FRAME_SIZE_KAMSTRUP_2101);
     printf("\n");
     for (int i = 0; i < MBUS_FRAME_SIZE_KAMSTRUP_2101; i++) {
-      printf("%0X ", data[i]);
+      printf("%0X ", mbus_data[i]);
       if ((i+1) % 32 == 0) {
         printf("\n");
       }
@@ -109,7 +109,7 @@ PROCESS_THREAD(hello_mbus_process, ev, data)
       memset(text_data[i], 0, sizeof(text_data[i]));
     }
 
-    mbus_parse_data_kamstrup_2101(data, text_data);
+    mbus_parse_data_kamstrup_2101(mbus_data, text_data);
 
 
     for (int i = 0; i < 37; i++)
