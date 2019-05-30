8c3675b109f3d16cfd5e17db6a218cb2406aa45a
Goradux <abbbo@mail.ru>
2019-05-30 13:06:51 +0200

    Added better explanation to the mbus example code

--- examples/avr-rss2/mbus/mbus-example.c
+++ examples/avr-rss2/mbus/mbus-example.c
@@ -29,8 +29,8 @@
  * This file is part of the Contiki operating system.
  *
  *
- * Author  : Robert Olsson robert@radio-sensors.com
- * Created : 2015-11-22
+ * Authors : Albert Asratyan and Mandar Joshi
+ * Created : 2019-05-30
  */
 
 /**
@@ -56,16 +56,12 @@ AUTOSTART_PROCESSES(&mbus_process);
 
 static struct etimer et;
 
-
-
-
-
-
 /*---------------------------------------------------------------------------*/
 PROCESS_THREAD(mbus_process, ev, data)
 {
   PROCESS_BEGIN();
 
+  // Initialize the UART1 port.
   usart1_init();
 
   leds_init();
@@ -82,52 +78,71 @@ PROCESS_THREAD(mbus_process, ev, data)
 
     leds_toggle(LEDS_YELLOW);
 
+    /* Comment in/out all of the code in each of the section to test it. */
+
+    //------------
+
     mbus_scan_primary_at_address(67);
+
+    //------------
+
     //mbus_scan_primary_all();
 
-
-
-
     //-------------
+
+    /*
+    //create an empty array that will hold the frame data
     uint16_t data[MBUS_FRAME_SIZE_KAMSTRUP_2101];
     memset(data, 0, sizeof(data));
+    //request the data with the empty array and expected frame size as arguments
     mbus_request_data_at_primary_address(67, data, MBUS_FRAME_SIZE_KAMSTRUP_2101);
     printf("\n");
+
+    //print out the received hex data
     for (int i = 0; i < MBUS_FRAME_SIZE_KAMSTRUP_2101; i++) {
       printf("%0X ", data[i]);
       if ((i+1) % 32 == 0) {
         printf("\n");
       }
     }
-
     printf("\n");
-    //
-    // watchdog_periodic();
-    //
-    // char text_data[37][32];
-    // //memset(text_data, 0, sizeof(text_data));
-    //
-    // for (int i = 0; i < 37; i++) {
-    //   memset(text_data[i], 0, sizeof(text_data[i]));
-    // }
-    //
-    // mbus_parse_data_kamstrup_2101(data, text_data);
-    //
-    //
-    // for (int i = 0; i < 37; i++)
-    // {
-    //   printf("%s\n", text_data[i]);
-    // }
+
+    watchdog_periodic();
+
+    //create a new array that will hold parsed data, specific to the flowIQ 2101
+    char text_data[37][32];
+    for (int i = 0; i < 37; i++) {
+      memset(text_data[i], 0, sizeof(text_data[i]));
+    }
+
+    //use the empty array as an argument that will be filled by the function
+    mbus_parse_data_kamstrup_2101(data, text_data);
+
+    //print out the parsed data
+    for (int i = 0; i < 37; i++)
+    {
+      printf("%s\n", text_data[i]);
+    }
+    */
+
     //-------------
 
-    // mbus_switch_baudrate(67, 2400);
-    //mbus_scan_primary_at_address(67);
-    // //
-    // mbus_set_primary_address(67, 100);
-    // mbus_scan_primary_at_address(67);
-    // mbus_scan_primary_at_address(100);
-    // mbus_set_primary_address(100, 67);
-    // mbus_scan_primary_at_address(67);
+    /*
+    mbus_switch_baudrate(67, 2400);
+    */
+
+    //-------------
+
+    /*
+    mbus_scan_primary_at_address(67);
+    mbus_set_primary_address(67, 100);
+    mbus_scan_primary_at_address(67);
+    mbus_scan_primary_at_address(100);
+    mbus_set_primary_address(100, 67);
+    mbus_scan_primary_at_address(67);
+    */
+
+    //-------------
 
 
     etimer_reset(&et);
